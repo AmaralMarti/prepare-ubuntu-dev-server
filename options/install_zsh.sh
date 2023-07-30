0e3bab6bec24df1280ea3c9114fad91e6875ff96
Henrique Marti <henrique.marti@puravida.com.br>
2023-07-30 01:59:57 +0000

    feat: block size

--- options/install_zsh.sh
+++ options/install_zsh.sh
@@ -3,7 +3,7 @@ install_zsh() {
     apt install -y git zsh
     wget --no-check-certificate http://install.ohmyz.sh -O - | sh
     sed -i 's/ZSH_THEME=".*"/ZSH_THEME="bira"/g' ~/.zshrc
-    echo "alias ll='ls -la'" >> ~/.zshrc
+    echo "alias ll='ls -la --block-size=k'" >> ~/.zshrc
     sudo usermod -s /usr/bin/zsh $USER
     source ~/.zshrc    
 }
